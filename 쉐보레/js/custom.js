d5b0b437ffcc5dfb0d41b1ddb1c3bcb6f374c2a4
kangsooji <148739333+kangsooji@users.noreply.github.com>
2023-11-10 14:42:15 +0900

    Add files via upload

---
+++ 쉐보레/js/custom.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+    });
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+
+})
